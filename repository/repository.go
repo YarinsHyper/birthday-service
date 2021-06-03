8859bb852e12e436522fc9bc66c8dfac49a6e770
yarinBenisty <yarin1857@gmail.com>
2021-06-03 13:29:31 +0300

    fix: working-methods-with correct layers

--- repository/repository.go
+++ repository/repository.go
@@ -1,3 +1,27 @@
 package repository
 
-type Repository struct{}
+import (
+	"fmt"
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type Repository struct{
+	c *mongo.Database;
+}
+
+func NewRepository(c *mongo.Database) *Repository{
+	return &Repository{c:c}
+}
+
+func (r *Repository)CreateBirthdayRepository(personObject bson.M){
+	collection := r.c.Collection("birthdays")
+	insertResult, err := collection.InsertOne(context.Background(), personObject)
+
+	if err != nil {	
+		fmt.Println("error: ", err)
+	}
+	fmt.Println("new birthday was created.\nbirthday id: ", insertResult.InsertedID)
+
+}
